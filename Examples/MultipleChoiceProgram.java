6d04b42611b629a1f86263f6b5ea5cfd9897465c
Akayashi Mika <github.xsn9j@akayashi.slmail.me>
2023-02-19 13:47:52 +0800

    Updated README.md

--- Examples/MultipleChoiceProgram.java
+++ Examples/MultipleChoiceProgram.java
@@ -95,9 +95,7 @@ public class MultipleChoiceProgram extends JFrame implements ActionListener {
             (questionNumber == 2 && option2.isSelected()) || 
             (questionNumber == 3 && option3.isSelected())) {
             score++;
-        }
-
-        questionNumber++;
+        } questionNumber++;
 
         if (questionNumber == questions.length) {
             JOptionPane.showMessageDialog(this, "Your score is " + score + " out of " + questions.length);
