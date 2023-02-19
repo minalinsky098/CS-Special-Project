9e046cb08cf9b6c5b1e4bdc7b1aefc529a4fa9e9
Akayashi Mika <github.xsn9j@akayashi.slmail.me>
2023-02-19 13:36:58 +0800

    Moved to Folder "Examples"

--- Examples/MultipleChoiceProgram.java
+++ Examples/MultipleChoiceProgram.java
@@ -1,3 +1,4 @@
+package Examples;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
--- Examples/WindowSwitch.java
+++ Examples/WindowSwitch.java
@@ -1,3 +1,4 @@
+package Examples;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
